b5f397bdeb579ea6c0c1bcea5cb239ed798ea68b
Lorena Morales <Lorenarm.999@gmail.com>
2022-10-26 13:03:02 -0600

    starter files and some boiler code

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server for ${activity} running on port ${PORT}!`);
+    });
+  });
