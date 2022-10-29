3b497e831231595c039a0dfa0925a772b9363a7c
Lorena Morales <Lorenarm.999@gmail.com>
2022-10-29 17:59:27 -0600

    reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,37 @@
 const { Schema, model } = require("mongoose");
 
+const reactionSchema = new Schema(
+    {
+       reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+       },
+       reactionBody: {
+            type: String, 
+            required: true, 
+            minLength: 1,
+            maxLength: 280,
+       },
+       username: {
+            type: String, 
+            required: true, 
+       },
+       createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => {
+                return new Date(timestamp).toLocaleString();
+            }
+       } 
+    },
+    {
+        toJSON: {
+        getters: true,
+        virtuals: true,
+        },
+        id: false,
+    }
+)
 
 const thoughtSchema = new Schema(
     {
@@ -11,7 +43,10 @@ const thoughtSchema = new Schema(
         },
         createdAt: {
             type: Date,
-            default: Date.now
+            default: Date.now,
+            get: (timestamp) => {
+                return new Date(timestamp).toLocaleString();
+            }
         },
         username: {
             type: String, 
@@ -27,3 +62,11 @@ const thoughtSchema = new Schema(
         id: false,
     }
 );
+
+thoughtSchema.virtual('reactionCount').get(function(){
+    return this.reactions.length
+});
+
+const Thought = model('thought', thoughtSchema);
+
+model.exports = Thought;
