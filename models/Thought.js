35c8db19f8c5ba4b8268277e2207caa9dafb1374
Lorena Morales <Lorenarm.999@gmail.com>
2022-10-29 17:47:30 -0600

    Thought Schema

---
+++ models/Thought.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require("mongoose");
+
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String, 
+            required: true, 
+            minLength: 1,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        username: {
+            type: String, 
+            required: true, 
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+        getters: true,
+        virtuals: true,
+        },
+        id: false,
+    }
+);
